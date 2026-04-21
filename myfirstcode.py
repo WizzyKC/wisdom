83f23bb7ed200be5ea1c5265d25777262fb5720b
wisdom <kelechiw67@gmail.com>
2026-04-21 13:10:41 +0100

    revenue and tax calculation

---
+++ myfirstcode.py
@@ -0,0 +1,9 @@
+revenue = 8000
+expenses = 2500
+tax_rate = 0.2
+profit_before_tax = revenue - expenses
+tax_amount = profit_before_tax - tax_rate
+profit_after_tax = profit_before_tax - tax_amount
+print("profit before tax is",profit_before_tax)
+print("tax amount is",tax_amount)
+print("profit after tax is",profit_after_tax)
